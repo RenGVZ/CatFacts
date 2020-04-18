8da945305febf76f5771c8e8bf02a7f5fd0e2bf0
gavingarza <gaving@live.com>
2020-04-18 22:58:11 +0900

    used an api to get 'cat facts'

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import 'bootstrap';
+import './scss/app.scss';
+
+const facts = document.querySelector("#facts");
+const listItem = document.querySelectorAll(".list-item");
+const submit = document.querySelector("#submit");
+
+const getFacts = () => {
+  fetch("https://catfact.ninja/fact")
+    .then(response => response.json())
+    .then((data) => {
+        listItem.forEach((item) => {
+          const apiResponse = `<li>${data.fact}</li>`;
+          item.insertAdjacentHTML("beforebegin", apiResponse);
+    });
+  });
+}
+getFacts()
