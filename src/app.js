fb41b84559596c8020213a0f759a4deba9990f1a
gavingarza <gaving@live.com>
2020-04-19 10:54:59 +0900

    added click function to generate api responses

--- src/app.js
+++ src/app.js
@@ -5,14 +5,17 @@ const facts = document.querySelector("#facts");
 const listItem = document.querySelectorAll(".list-item");
 const submit = document.querySelector("#submit");
 
-const getFacts = () => {
-  fetch("https://catfact.ninja/fact")
-    .then(response => response.json())
-    .then((data) => {
-        listItem.forEach((item) => {
-          const apiResponse = `<li>${data.fact}</li>`;
-          item.insertAdjacentHTML("beforebegin", apiResponse);
+submit.addEventListener('click', function() {
+
+  function getFacts() {
+    fetch("https://catfact.ninja/fact")
+      .then(response => response.json())
+      .then((data) => {
+          listItem.forEach((item) => {
+            const apiResponse = `<li>${data.fact}</li>`;
+            item.insertAdjacentHTML("beforebegin", apiResponse);
+      });
     });
-  });
-}
-getFacts()
+  };
+  getFacts();
+});
